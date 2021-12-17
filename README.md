c348b3850e2c4c63b9c33f4a8b33dd8bb1c7ece5
samuelbarrett <72281080+sirsamuelb@users.noreply.github.com>
2021-12-17 15:24:39 -0600

    add sqlite:JDBC for db interaction

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 <text>Group project for COMP 3380 Fall 2021 (group 6)</text>
 
 <h3>Compile and run:</h3>
-<text>[here are the instructions]</text>
+<text>javac SubstanceUse.java</text>
+<text>java -cp .:</text>
 
 <text>Built using Java and JDBC for SQLite, Swing GUI</text>
 
--- SubstanceUse.java
+++ SubstanceUse.java
@@ -4,3 +4,53 @@
 // COMP 3380
 // Project Team 6
 
+// notes: this will be the class with jdbc and methods interacting with our sqlite file.
+// 		we will create an instance of this class and call its methods in another java file which will contain our main method.
+//		that file will also have our UI (or at least calls the UI).
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SubstanceUse {
+
+	// constructor
+	public SubstanceUse() {
+
+	}
+
+	// main
+	public static void main(String[] args) {
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		Connection connect = null;
+		try {
+			// connect to the database file (substance-use-wpg.sqlite)
+			connect = DriverManager.getConnection("jdbc:sqlite:/substance-use-wpg.sqlite");
+
+			// everything else goes here (loop basically? until we exit the program?)
+
+		} 
+		catch(SQLException e) {
+			System.out.println(e.getMessage());
+		} 
+		finally {
+			// we close the connection if applicable
+			try {
+				if(connect != null) {
+					connect.close();
+				}
+			} catch(SQLException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+
+
+
+}
